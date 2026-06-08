129fc2a851d4382a5c271da57433846b6790dcd1
SmilingDogs <a.kudr74@gmail.com>
2026-06-08 19:07:26 +0300

    Skills & Exper section done

--- frontend_portfolio/src/container/Skills/Skills.jsx
+++ frontend_portfolio/src/container/Skills/Skills.jsx
@@ -1,7 +1,79 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { Tooltip } from "react-tooltip";
+import { client, urlFor } from "../../client";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Skills.scss";
+const experiencesQuery = '*[_type == "experiences"]';
+const skillsQuery = '*[_type == "skills"]';
 
-const Skills = () => {
-  return <div>Skills</div>;
+const SkillsContent = () => {
+  const [skills, setSkills] = useState([]);
+  const [experiences, setExperiences] = useState([]);
+
+  useEffect(() => {
+    client.fetch(experiencesQuery).then((data) => setExperiences(data));
+    client.fetch(skillsQuery).then((data) => setSkills(data));
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">Skills & Experiences</h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill._id}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience._id}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <motion.div
+                    key={work._key || work.name}
+                    whileInView={{ opacity: [0, 1] }}
+                    transition={{ duration: 0.5 }}
+                    className="app__skills-exp-work"
+                    data-tooltip-id="work-tooltip"
+                    data-tooltip-content={work.desc}
+                  >
+                    <h4 className="bold-text">{work.name}</h4>
+                    <p className="text-company">{work.company}</p>
+                    <p className="p-text">{work.desc}</p>
+                  </motion.div>
+                ))}
+                <Tooltip id="work-tooltip" className="skills-tooltip" />
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
+
+const Skills = AppWrap(
+  MotionWrap(SkillsContent, "app__skills"),
+  "skills",
+  "app__whitebg",
+);
 
 export default Skills;
--- frontend_portfolio/src/wrapper/AppWrap.jsx
+++ frontend_portfolio/src/wrapper/AppWrap.jsx
@@ -3,15 +3,14 @@ import { NavigationDots, SocialMedia } from "../components";
 const AppWrap = (Component, sectionName, ...classNames) =>
   function HOC() {
     return (
-      <div id={sectionName} className={`app__container ${classNames.join(' ')}`}>
+      <div
+        id={sectionName}
+        className={`app__container ${classNames.join(" ")}`}
+      >
         <SocialMedia />
 
         <div className="app__wrapper app__flex">
           <Component />
-          <div className="copyright">
-            <p className="p-text">@2026 OLEKSIY</p>
-            <p className="p-text">All rights reserved</p>
-          </div>
         </div>
         <NavigationDots active={sectionName} />
       </div>
